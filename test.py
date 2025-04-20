0ee0e8635c661e7116ad507997c5dbefe11b0540
Jeffrey <jeffk613@gmail.com>
2025-04-19 22:07:52 -0400

    Got the loraify to not cause exceptions. Inference still has some kinks to work out.

--- test.py
+++ test.py
@@ -1,30 +1,48 @@
-from argparse import ArgumentParser
+import sys
+from jutils.utils import add_module_to_path, pdb
+from jutils.nn_utils import loraify
+add_module_to_path("fbdetr")
 import torch
-from fbdetr.datasets import build_dataset, build_multicam, build_multicam_dataloader
+from fbdetr.datasets import build_dataset
 import fbdetr.util.misc as utils
 from fbdetr.models import build_model
 from fbdetr.main import get_args_parser
-from jutils.jnn import LoRA
-from jutils.nn_utils import loraify
 from torch.utils.data import DataLoader
-#TODO: change the build function, because you need to change the number of output classes.
-#TODO: moreover, start looking into low rank approximation.
-#TODO: hmm. Starting to think that I should just copy and paste the  argparse in main.py.
 
 def get_weights(args):
     # args.frozen weights is the path to the checkpoint
-    checkpoint = torch.load(args.frozen_weights, map_location='cpu')
+    checkpoint = torch.hub.load_state_dict_from_url(args.resume, map_location='cpu')
     return checkpoint['model']
 
 
 def test_detr_loraify():
-    args = get_args_parser()
+    print(sys.path)
+    parser = get_args_parser()
+    args = parser.parse_args()
+    args.batch_size = 2
+    args.no_aux_loss = True
+    args.eval = True
+    args.resume = "https://dl.fbaipublicfiles.com/detr/detr-r50-e632da11.pth"
+    args.coco_path = "erwiam_dataset"
+    args.image_set = "train"
+    args.dataset_file = "fire_blight"
+    args.masks = False
+    args.device = "cuda"
+    ### args stuff mostly done
     dataset = build_dataset(args.image_set, args)
     dataloader = DataLoader(dataset, batch_size=4, collate_fn=utils.collate_fn)
+    args.dataset_file = "coco"
     detr, *_ = build_model(args)
     weights = get_weights(args)
     detr.load_state_dict(weights)
     detr_lora = loraify(detr)
     for b in dataloader:
+        pdb()
         pred = detr_lora(b)
         print(pred)
+
+def main():
+    test_detr_loraify()
+
+if __name__ == "__main__":
+    main()
