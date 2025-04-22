402eca2b62ce8b5dd107b4d47da46a78f5bc8dc4
Jeffrey <jeffk613@gmail.com>
2025-04-21 21:37:42 -0400

    Added a run script.

---
+++ run.sh
@@ -0,0 +1,8 @@
+MODE=$1
+if [ "$MODE" == "train" ]; then
+    python fbdetr/main.py --coco_path erwiam_dataset --dataset_file fire_blight --output_dir checkpoints --resume "https://dl.fbaipublicfiles.com/detr/detr-r50-e632da11.pth" 
+elif [ "$MODE" == "eval" ]; then
+    python fbdetr/main.py --batch_size 2 --no_aux_loss --eval --resume https://dl.fbaipublicfiles.com/detr/detr-r50-e632da11.pth --coco_path erwiam_dataset --dataset_file fire_blight
+elif [ "$MODE" == "vis" ]; then
+    python vis.py --coco_path erwiam_dataset --dataset_file fire_blight
+fi
