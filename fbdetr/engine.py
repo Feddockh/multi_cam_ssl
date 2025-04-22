9e28274e3cf6cb689c233e593fbea868573c8a7d
Jeffrey <jeffk613@gmail.com>
2025-04-21 21:36:32 -0400

    Added gradient and loss logging from within the train_one_epoch function.

--- fbdetr/engine.py
+++ fbdetr/engine.py
@@ -12,8 +12,8 @@ import torch
 import util.misc as utils
 from datasets.coco_eval import CocoEvaluator
 from datasets.panoptic_eval import PanopticEvaluator
-
-
+from jutils.logger import get_writer, get_step, log_gradients
+from jutils.utils import pdb
 def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
                     data_loader: Iterable, optimizer: torch.optim.Optimizer,
                     device: torch.device, epoch: int, max_norm: float = 0):
@@ -28,12 +28,12 @@ def train_one_epoch(model: torch.nn.Module, criterion: torch.nn.Module,
     for samples, targets in metric_logger.log_every(data_loader, print_freq, header):
         samples = samples.to(device)
         targets = [{k: v.to(device) for k, v in t.items()} for t in targets]
-
         outputs = model(samples)
         loss_dict = criterion(outputs, targets)
         weight_dict = criterion.weight_dict
         losses = sum(loss_dict[k] * weight_dict[k] for k in loss_dict.keys() if k in weight_dict)
-
+        get_writer().add_scalar("losses", losses, get_step("losses"))
+        log_gradients(model, get_step("losses"))
         # reduce losses over all GPUs for logging purposes
         loss_dict_reduced = utils.reduce_dict(loss_dict)
         loss_dict_reduced_unscaled = {f'{k}_unscaled': v
